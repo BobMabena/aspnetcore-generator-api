9e55370dc11787ecb3846d8c65ac57c483b35b20
Greg McDavid <Greg.McDavid@ipreo.com>
2017-10-06 14:15:50 +0200

    added sort test

--- tests/RangeTests.cs
+++ tests/RangeTests.cs
@@ -16,5 +16,16 @@ namespace test
 
             Assert.Equal(3, generated.Count());
         }
+
+        [Fact]
+        public void SortShouldOrderResults()
+        {
+            var range = new Range {Count =3, Sort = true};
+            var values = new[] {"a","b","c"};
+            var counter = 0;
+            var generated = range.Of(() => values[counter++]);
+
+            Asser.Equal(new [] {"a","b","c"},generated.ToArray());
+        }
     }
 }
